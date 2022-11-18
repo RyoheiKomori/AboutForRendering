94629f2491f93a4ad3bf24ff0f3d54c3258a78dc
RyoheiKomori <59478903+RyoheiKomori@users.noreply.github.com>
2022-11-18 19:37:24 +0900

    complete

--- src/ChildArea.jsx
+++ src/ChildArea.jsx
@@ -1,11 +1,13 @@
+import { memo } from "react";
+
 const style = {
   width: "100%",
   height: "200px",
   backgroundColor: "khaki"
 };
 
-export const ChildArea = (props) => {
-  const { open } = props;
+export const ChildArea = memo((props) => {
+  const { open, onClickClose } = props;
 
   const data = [...Array(2000).keys()];
   data.forEach(() => {
@@ -17,8 +19,9 @@ export const ChildArea = (props) => {
       {open ? (
         <div style={style}>
           <p>子コンポーネント</p>
+          <button onClick={onClickClose}>閉じる</button>
         </div>
       ) : null}
     </>
   );
-};
+});
